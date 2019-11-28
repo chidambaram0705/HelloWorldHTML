ff0d848796b94848aef5372bf2d93d807e4c1f01
chidambaram0705 <chidambaram0705@gmail.com>
2019-11-28 12:34:04 +0530

    Added PHP command

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,11 +1,3 @@
-<html>
-<head></head>
-<body>
-<div style="height:80%;width:100%;background-color:#ddd;padding:30px 0px 0px 30px;">
-<h2>Hello World<h2>
-</div>
-<div style="float:right;font-size:10px;">
-<span>Chidambaram Subramanian</span>
-</div>
-</body>
-</html>
+<?php
+echo "Hello World";
+?>
